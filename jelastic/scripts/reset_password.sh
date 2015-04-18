21e2d0a4536a6ddd484cf6cbab863cea3cec864f
SiryjVyiko <dz@hivext.net>
2015-04-18 14:03:21 +0300

    Update reset_password.sh

--- jelastic/scripts/reset_password.sh
+++ jelastic/scripts/reset_password.sh
@@ -7,7 +7,7 @@ export JAVA_HOME="/usr/java/default/";
 #$J_OPENSHIFT_APP_ADM_PASSWORD    ;   Use this varible for your password
 
 function _setPassword() {
-        sed -i '/admin/d' ${CARTRIDGE_HOME}/versions/${Version}/standalone/configuration/mgmt-users.properties;
+        sed -i '/^admin=/d' ${CARTRIDGE_HOME}/versions/${Version}/standalone/configuration/mgmt-users.properties;
         pass_hash=`echo -n ${J_OPENSHIFT_APP_ADM_USER}:ManagementRealm:${J_OPENSHIFT_APP_ADM_PASSWORD}|md5sum|awk '{print $1}'`;
         echo ${J_OPENSHIFT_APP_ADM_USER}=${pass_hash} >> ${CARTRIDGE_HOME}/versions/${Version}/standalone/configuration/mgmt-users.properties;
 }
